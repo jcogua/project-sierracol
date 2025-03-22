37900f443aa8dc24ea26a4d05c686efd33f9f57c
Jesus Cogua <jes.cogm@gmail.com>
2025-03-22 16:44:04 -0500

    Setting render con docker

---
+++ src/main.py
@@ -0,0 +1,13 @@
+import prefect
+from prefect import flow
+from extraction.api_extraction import extract_api_data
+from extraction.opendata_extraction import extract_open_data
+
+@flow(name="Main Pipeline Flow")
+def main_pipeline():
+    api_data = extract_api_data()
+    open_data = extract_open_data()
+    # Aquí puedes agregar transformaciones y carga a la base de datos
+
+if __name__ == "__main__":
+    main_pipeline()
