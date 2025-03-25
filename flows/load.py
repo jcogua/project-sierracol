b8758825d32bfc0d90cea0fcfbeb6ef4386d8bc5
Jesus Cogua <jes.cogm@gmail.com>
2025-03-25 15:33:28 -0500

    Ajuste

--- flows/load.py
+++ flows/load.py
@@ -9,11 +9,11 @@ def load_to_postgres(df_api, df_excel, df_csv):
     logger = get_run_logger()
     try:
         engine = create_engine(DATABASE_URL)
-        df_api.to_sql("petroleum_prices", engine, if_exists="append", index=False)
+        df_api.to_sql("petroleum_prices", engine, if_exists="replace", index=False)
         notify_slack(f"✅ Database load successful - petroleum_prices")
-        df_excel.to_sql("energy_metrics", engine, if_exists="append", index=False)
+        df_excel.to_sql("energy_metrics", engine, if_exists="replace", index=False)
         notify_slack(f"✅ Database load successful - energy_metrics")
-        df_csv.to_sql("geogist", engine, if_exists="append", index=False)
+        df_csv.to_sql("geogist", engine, if_exists="replace", index=False)
         notify_slack(f"✅ Database load successful - GEOGIST")
         with engine.connect() as conn:
             conn.execute(text("CREATE INDEX IF NOT EXISTS idx_energy_country ON energy_metrics (country)"))
