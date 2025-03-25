29f400499fa4cef2756039ee353b8ac38a9655be
Jesus Cogua <jes.cogm@gmail.com>
2025-03-24 19:00:53 -0500

    Setting render con docker

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from flows.etl import build_etl_pipeline
 
 if __name__ == "__main__":
     print("Entró a __main__")
-    build_etl_pipeline()
+    etl_pipeline = build_etl_pipeline()
+    etl_pipeline()  # Aquí lo ejecutas realmente
